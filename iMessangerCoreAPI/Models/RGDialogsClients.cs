b391e67f49898311a2de2c61253ee022d307f3e2
Liza <bliza0812@GMAIL.COM>
2022-09-16 02:48:12 +0300

    Добавьте файлы проекта.

---
+++ iMessangerCoreAPI/Models/RGDialogsClients.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace iMessangerCoreAPI.Models
+{
+    public class RGDialogsClients
+    {
+        public Guid IDUnique { get; set; }
+
+
+        public Guid IDRGDialog { get; set; }
+        public Guid IDClient { get; set; }
+        public DateTime? DateEvent { get; set; }
+
+    }
+}
